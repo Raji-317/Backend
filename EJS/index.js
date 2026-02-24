12442753fc07969fb93274ba7fcc9a5ae2a1ada8
Raji@317 <rajyalakshmigangadhar563@gmail.com>
2026-02-24 20:18:05 +0530

    initial message

--- EJS/index.js
+++ EJS/index.js
@@ -2,16 +2,31 @@ const express=require('express');
 const app=express()
 const path=require('path');
 const port=3000;
+
+
+// app.use():it is a function whichh is used for to import teh static files from teh differnt folders
+app.use(express.static(path.join(__dirname,"public")));//connect the css file from the public folder
+// if we want to connect the other files then we have to get them like this if it is static file..
+
+
+//if we want to import dynamic files then we use it
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"view"));
 
+
+
+//to send the response back from server to the client we use this method
 app.get("/app", (req, res) => {
     res.send("Hello");
 });
+
+
 //rendering the pages
 app.get("/",(req,res)=>{
     res.render("home.ejs");
 });
+
+
 
 app.get("/random",(req,res)=>{
     // if we want to we can also write the math.random( function here also ..for now we have written in the .ejs file only)//
@@ -20,11 +35,24 @@ app.get("/random",(req,res)=>{
 });
 
 
-//instgram interface:
+//instgram interface:using teh data that we have stored in the array
+// app.get("/instagram/:username",(req,res)=>{
+//     const followers=["Raji","ABHI","Dhamini","Gangadhar","srinivas","Srihari"];
+//     let {username}=req.params;
+//     res.render("instagram",{username,followers});
+// });
+
+//now using the data from the database and rendering the page:
 app.get("/instagram/:username",(req,res)=>{
-    const followers=["Raji","ABHI","Dhamini","Gangadhar","srinivas","Srihari"];
     let {username}=req.params;
-    res.render("instagram",{username,followers});
+    const instadata=require("./data.json");
+    const data=instadata[username];
+    if(data){
+        res.render("instagram.ejs",{data});
+    }
+    else{
+        res.render("error.ejs");
+    }
 });
 
 app.listen(port,()=>{
